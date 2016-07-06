2eab848624f40cbe692d5f371c62a104782ec077
ᔕᗩYᗩᑎ GOᔕᗯᗩᗰI <Sayan98@users.noreply.github.com>
2016-07-06 14:45:09 +0530

    Minified code and removed useless calls.
    Multi-threading to come next for faster downloads.

--- earth_view_dl.py
+++ earth_view_dl.py
@@ -1,6 +1,5 @@
 from urllib.request import urlopen,urlretrieve
 from urllib.error import HTTPError
-from bs4 import BeautifulSoup
 import time,os
 
 for x in range(1000, 7030):
@@ -9,11 +8,6 @@ for x in range(1000, 7030):
         response = urlopen('https://earthview.withgoogle.com/' + x)
         if response.getcode() == 200:
             start = time.time()
-            
-            data = response.read()
-            html = BeautifulSoup(data,"html.parser")
-            Everything = html.find("a", id="globe", href=True)
-            GMapsURL = Everything['href']
             Image = 'https://www.gstatic.com/prettyearth/assets/full/' + x + '.jpg'
             if not os.path.isfile(os.path.join('images',Image.split('/')[-1])) : # for download resume
                 urlretrieve(Image, os.path.join('images',Image.split('/')[-1]) )
