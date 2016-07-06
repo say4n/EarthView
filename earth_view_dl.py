9c4d8c1fd9aa51d96d008d7a99084972aa75a0cb
ᔕᗩYᗩᑎ GOᔕᗯᗩᗰI <Sayan98@users.noreply.github.com>
2016-07-06 14:48:24 +0530

    Better download resumption.

--- earth_view_dl.py
+++ earth_view_dl.py
@@ -11,8 +11,9 @@ for x in range(1000, 7030):
             Image = 'https://www.gstatic.com/prettyearth/assets/full/' + x + '.jpg'
             if not os.path.isfile(os.path.join('images',Image.split('/')[-1])) : # for download resume
                 urlretrieve(Image, os.path.join('images',Image.split('/')[-1]) )
-
-            print("Fetched -> | " + x + " | in " + "{0:.3f}".format(time.time() - start) + " sec" )
+                print("Fetched -> | " + x + " | in " + "{0:.4f}".format(time.time() - start) + " sec" )
+            else :
+                print("Already fetched! -> | " + x + " | ")
     
     except (HTTPError,AttributeError):
         continue #If the page is 404, then just continue
